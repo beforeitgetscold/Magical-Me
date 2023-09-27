d45ad7d12ee463558bf9e4f9b1c66244342fd350
beforeitgetscold <143422165+beforeitgetscold@users.noreply.github.com>
2023-09-26 21:19:35 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function moneyAnswer(){
-  var password = document.getElementById("partyQuestion");
-  var passwordText = password.value;
-  if(passwordText=="Democratic"){
-    return true; 
-  }
-  alert("L. Try Again")
-  return false;
-}
